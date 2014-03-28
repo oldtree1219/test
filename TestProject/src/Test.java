a6028d5a4f9eda0701afbfee49a751202c503783
jhta <jhta@L3-24>
2014-03-28 14:37:19 +0900

    출력기능추가

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -2,7 +2,8 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated methodhhhhhhhh
+		// TODO Auto-generated methodhhhhhhhh]
+		System.out.println("¾È³ç¤©¾È³ç¹Ý°¡¿ö");
 
 	}
 
