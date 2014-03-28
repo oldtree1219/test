a6a0f5118d870cee095f50261981ae576ae073f0
jhta <jhta@L3-24>
2014-03-28 15:12:43 +0900

    utf-8설정

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated methodhhhhhhhh]
-		System.out.println("3월21일 최고의 날");
+		System.out.println("4444444444");
 
 	}
 
