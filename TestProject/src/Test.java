7f477e1696c080a9de4e7bb062a09806ab0aae5e
jhta <jhta@L3-24>
2014-03-28 15:13:24 +0900

    utf-8 한글확인

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated methodhhhhhhhh]
-		System.out.println("4444444444");
+		System.out.println("utf-8 한글로 출력시키기");
 
 	}
 
